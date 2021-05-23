bf38bbe52401dab8055c1ae475fb08c2c857a231
D3vLuca <lucalana13@gmail.com>
2021-05-22 23:04:23 -0300

    implementação

--- src/Calculadora/visao/Botao.java
+++ src/Calculadora/visao/Botao.java
@@ -1,6 +1,16 @@
 package Calculadora.visao;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Botao extends JButton {
+
+    public Botao (String texto , Color cor){
+        setText(texto);
+        setOpaque(true);
+        setBackground(cor);
+        setForeground(Color.white);
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        setFont(new Font("courier",Font.PLAIN,25));
+    }
 }
--- src/Calculadora/visao/Display.java
+++ src/Calculadora/visao/Display.java
@@ -1,11 +1,23 @@
 package Calculadora.visao;
 
 import javax.swing.*;
+import java.awt.*;
 
 
 public class Display extends JPanel {
 
+    private final JLabel label;
+
+
     public Display() {
 
+      label = new JLabel("1234,56");
+      setBackground(new Color(46,49,50));
+      label.setForeground(Color.WHITE);
+      label.setFont(new Font("courier",Font.PLAIN,30));
+
+      setLayout(new FlowLayout(FlowLayout.RIGHT,10,25));
+
+      add(label);
     }
 }
